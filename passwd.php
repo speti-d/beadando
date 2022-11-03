2a049180eeea720b81b65eb0b0b2e02a38ec24e4
Peter Soltesz <solteszpeter98@gmail.com>
2022-11-03 03:06:58 +0100

    decrypt function done

--- passwd.php
+++ passwd.php
@@ -1,13 +1,37 @@
 <?php
     function login($email, $passwd){
-        $sajt = 'rak';
+        //$passwords = open_pwds();
+        $password_file = fopen('password.txt', 'r');
+
+        return decrypt(fgets($password_file));
+    }
+
+    // Non functional
+    function open_pwds(){
         if(file_exists('password.txt')){
             $password_file = fopen('password.txt', 'r');
-            $sajt = '';
             while(!feof($password_file)){
-                $sajt .= fgets($password_file);
+                $line = decrypt(fgets($password_file));
             }
         }
-        return $sajt;
+        return 'a return value';
     }
+
+    function decrypt($secret){
+        $decrypted = '';
+        global $KEY_ARRAY;
+        for ($i=0; $i < strlen($secret) - 1; $i++) { 
+            $decrypted .= chr((ord(substr($secret, $i)) - $KEY_ARRAY[$i % 5])  % 127);
+        }
+            
+        return $decrypted;
+    }
+
+    $KEY_ARRAY = [
+        0 => 5,
+        1 => -14,
+        2 => 31,
+        3 => -9,
+        4 => 3,
+    ]
 ?>
