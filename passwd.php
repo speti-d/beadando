3a95af7da7c6f1965ca0cbd8e7662adce286f501
Peter Soltesz <solteszpeter98@gmail.com>
2022-11-03 01:37:21 +0100

    Opened pwd.txt, printed

--- passwd.php
+++ passwd.php
@@ -1,7 +1,13 @@
 <?php
-    $sajt = 'geci';
     if($_POST['e-mail'] != '' and $_POST['passwrd'] != '')
     {
         $sajt = 'gyasz';
+        if(file_exists('password.txt')){
+            $password_file = fopen('password.txt', 'r');
+            $sajt = '';
+            while(!feof($password_file)){
+                $sajt .= fgets($password_file);
+            }
+        }
     }
 ?>
